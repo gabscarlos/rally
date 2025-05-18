0f1e8c3f7abfffbd91141ff16d9563ceaeb17811
gabscarlos <gabrielcarllos@yahoo.com.br>
2025-05-17 22:09:50 -0300

    analytics

--- src/app/(paginas)/layout.tsx
+++ src/app/(paginas)/layout.tsx
@@ -1,9 +1,15 @@
 import Pagina from "@/components/template/Pagina";
+import { Analytics } from "@vercel/analytics/next";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout(props: LayoutProps) {
-  return <Pagina>{props.children}</Pagina>;
+  return (
+    <Pagina>
+      <Analytics />
+      {props.children}
+    </Pagina>
+  );
 }
